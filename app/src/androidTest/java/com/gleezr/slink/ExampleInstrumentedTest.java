2d8869ac63d08c24bc5728c1edbf6f595f348f55
Erez <erez.lankri@gleezr.com>
2016-10-20 18:03:26 +0300

    Implemented Shared Preferences interfaces

--- app/src/androidTest/java/com/gleezr/slink/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/gleezr/slink/ExampleInstrumentedTest.java
@@ -1,12 +1,17 @@
 package com.gleezr.slink;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +21,37 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.gleezr.slink", appContext.getPackageName());
+    static Context cntxt;
+    static  SharedPreferences slnk;
+    static SharedPreferences.Editor edt;
+
+    @BeforeClass
+    public static void useAppContext() throws Exception {
+        cntxt = InstrumentationRegistry.getTargetContext();
+
+        slnk = new Slink2(new File(cntxt.getFilesDir() + "Test1"),cntxt.MODE_PRIVATE, cntxt);
+
+        edt = slnk.edit();
+    }
+
+    @Test
+    public void saveIntegerValueUsingCommit()
+    {
+        edt.putInt("MyFirstInteger", 1);
+
+        edt.commit();
+
+        assertEquals(1,slnk.getInt("MyFirstInteger", 9));
+    }
+
+    @Test
+    public void saveIntegerValueUsingApply()
+    {
+        edt.putInt("MyFirstInteger", 1);
+
+        edt.apply();
+
+        assertEquals(1,slnk.getInt("MyFirstInteger", 9));
     }
 }
